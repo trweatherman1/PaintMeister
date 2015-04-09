3d5b56a40940e7117f17d7d2799523f263da9a2f
Trent <trweatherman1@catamount.wcu.edu>
2015-04-09 12:04:38 -0400

    splash screen nigga

--- app/src/main/java/com/example/trent/paintmeister/SplashScreen.java
+++ app/src/main/java/com/example/trent/paintmeister/SplashScreen.java
@@ -1,39 +1,44 @@
 package com.example.trent.paintmeister;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 
 public class SplashScreen extends ActionBarActivity {
+
+    ProgressBar progress;
+    android.os.Handler handler;
+    static final int DELAY = 5000;
+    Class nextActivity = MainMenu.class;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        progress = (ProgressBar)this.findViewById(R.id.progressBar);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
-        return true;
+    public void onStart(){
+        super.onStart();
+        handler = new android.os.Handler();
+        handler.postDelayed(r, DELAY);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    Runnable r = new Runnable(){
+        public void run(){
+            goToNextScreen();
         }
+    };
 
-        return super.onOptionsItemSelected(item);
+    public void goToNextScreen(){
+        Intent nextScreen = new Intent(this,nextActivity);
+        this.startActivity(nextScreen);
+        finish();
     }
+
+
 }
